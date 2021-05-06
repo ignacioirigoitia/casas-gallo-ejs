3f7f8af61b496528a498dce5bac0ed7abe410150
ignacioirigoitia <ignacio.irigoitia@gmail.com>
2021-05-06 20:56:45 -0300

    termine puerto-gallo

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -3,7 +3,10 @@ module.exports = {
         res.render('index', { title: 'Tu Hospedaje Delta' })
     },
     puertoGallo: (req, res) => {
-        res.render("puertoGallo", { title: "Puerto Gallo" })
+
+        let imagenes = ["puertogallo1.jpg","puertogallo3.jpg","puertogallo4.jpg","puertogallo5.jpg","puertogallo6.jpg","puertogallo7.jpg","puertogallo8.jpg","puertogallo9.jpg","puertogallo10.jpg","puertogallo11.jpg","puertogallo12.jpg","puertogallo13.jpg","puertogallo14.jpg","puertogallo15.jpg","puertogallo16.jpg","puertogallo2.jpg",]
+
+        res.render("puertoGallo", { title: "Puerto Gallo", imagenes })
     },
     timbu: (req, res) => {
         res.render("timbu", { title: "Timbu" })
